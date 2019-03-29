c9a2034a36c90281d119046678c99fa6fdd2952b
Chris Mutel <cmutel@gmail.com>
2019-03-29 18:40:53 +0100

    Add another function that doesn't convert to exiobase

--- beebee/convert_entsoe_to_numpy.py
+++ beebee/convert_entsoe_to_numpy.py
@@ -64,6 +64,69 @@ FLOW_MAPPING = {
 def convert_entsoe_to_numpy(year, rdf_base_dir):
     metadata = get_metadata(rdf_base_dir)
 
+    nrows = len(COUNTRIES) * len(set(FLOW_MAPPING))
+    ncols = len(COUNTRIES) * len(set(ACTIVITY_MAPPING))
+
+    supply = np.zeros((nrows, ncols), dtype=np.float32)
+    use = np.zeros((nrows, ncols), dtype=np.float32)
+
+    sorted_flow_values = sorted(set(FLOW_MAPPING))
+    sorted_activity_values = sorted(set(ACTIVITY_MAPPING))
+    sorted_countries = sorted(COUNTRIES)
+
+    col_mapping = {
+        (x, y): i for i, (x, y) in enumerate(itertools.product(
+        sorted_countries, sorted_activity_values))
+    }
+    row_mapping = {
+        (x, y): i for i, (x, y) in enumerate(itertools.product(
+        sorted_countries, sorted_flow_values))
+    }
+
+    # Get grid mixes
+    for technology, country, amount in iterate_generation(year):
+        supply[
+            row_mapping[(country, technology)],
+            col_mapping[(country, technology)],
+        ] += amount
+
+    for technology, country, amount in iterate_generation(year):
+        use[
+            row_mapping[(country, technology)],
+            col_mapping[(country, 'Grid')],
+        ] += amount
+
+    for from_, to_, amount in iterate_trade(year):
+        use[
+            row_mapping[(from_, 'Grid')],
+            col_mapping[(to_, 'Grid')],
+        ] += amount
+
+    with tempfile.TemporaryDirectory() as t:
+        output_dir = Path(t)
+
+        np.save(output_dir / "entsoe-supply.npy", supply)
+        np.save(output_dir / "entsoe-use.npy", supply)
+
+        rm_reverse = sorted([(v, k) for k, v in row_mapping.items()])
+        cm_reverse = sorted([(v, k) for k, v in col_mapping.items()])
+        with open(output_dir / "entsoe-products.json", "w", encoding='utf-8') as f:
+            json.dump([x[1] for x in rm_reverse], f, ensure_ascii=False)
+        with open(output_dir / "entsoe-activities.json", "w", encoding='utf-8') as f:
+            json.dump([x[1] for x in cm_reverse], f, ensure_ascii=False)
+
+        with tarfile.open("entsoe-beebee-{}.tar.bz2".format(year),
+                          "w:bz2") as f:
+            f.add(output_dir / "entsoe-supply.npy", "entsoe-supply.npy")
+            f.add(output_dir / "entsoe-use.npy", "entsoe-use.npy")
+            f.add(output_dir / "entsoe-activities.json", "entsoe-activities.json")
+            f.add(output_dir / "entsoe-products.json", "entsoe-products.json")
+
+
+def convert_entsoe_to_numpy_exiobase(year, rdf_base_dir):
+    metadata = get_metadata(rdf_base_dir)
+
+    # Use mapping to exiobase
     nrows = len(COUNTRIES) * len(set(FLOW_MAPPING.values()))
     ncols = len(COUNTRIES) * len(set(ACTIVITY_MAPPING.values()))
     supply = np.zeros((nrows, ncols), dtype=np.float32)
